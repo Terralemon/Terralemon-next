e9747bcf13ea084dba18e41257459f0dd5429098
Harold Kuiper <haroldkuiper@Harolds-Mac-mini.local>
2025-03-17 16:52:05 +0100

    Update referenties naar favicon en og-image naar externe URLs

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,12 +13,12 @@ export default function Layout({ children, title, description }) {
         <meta name="description" content={pageDescription} />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
-        <meta property="og:image" content="/img/general/terralemon-twitter.jpg" />
+        <meta property="og:image" content="https://terralemon.nl/img/general/terralemon-twitter.jpg" />
         <meta property="og:type" content="website" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content={pageTitle} />
         <meta property="twitter:description" content={pageDescription} />
-        <meta property="twitter:image" content="/img/general/terralemon-twitter.jpg" />
+        <meta property="twitter:image" content="https://terralemon.nl/img/general/terralemon-twitter.jpg" />
       </Head>
       <Navbar />
       <main>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,11 +13,11 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
           rel="stylesheet"
         />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b1dd" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://terralemon.nl/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://terralemon.nl/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://terralemon.nl/favicon-16x16.png" />
+        <link rel="manifest" href="https://terralemon.nl/site.webmanifest" />
+        <link rel="mask-icon" href="https://terralemon.nl/safari-pinned-tab.svg" color="#00b1dd" />
         <meta name="msapplication-TileColor" content="#ff0000" />
         <meta name="theme-color" content="#00b1dd" />
       </Head>
